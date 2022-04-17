7e69208553c0e85bdf027a79052fb7ae39507924
Михаил <miha061103@gmail.com>
2022-04-17 23:22:20 +0500

    test

--- editor.py
+++ editor.py
@@ -1,1 +1,55 @@
-print('this is the editor')
+#print('this is the editor')
+import random
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPainter
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
+
+import sys  # Только для доступа к аргументам командной строки
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Points')
+        self.show()
+
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+        self.drawPoints(qp)
+        qp.end()
+
+
+    def drawPoints(self, qp):
+
+        qp.setPen(Qt.GlobalColor.red)
+        size = self.size()
+
+        for i in range(1000):
+            x = random.randint(1, size.width()-1)
+            y = random.randint(1, size.height()-1)
+            qp.drawPoint(x, y)
+
+
+
+if __name__ == "__main__":
+    # Приложению нужен один (и только один) экземпляр QApplication.
+    # Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
+    # Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
+    app = QApplication(sys.argv)
+
+    # Создаём виджет Qt — окно.
+    ex = Example()
+
+    app.exec()

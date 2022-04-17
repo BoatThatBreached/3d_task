5a0a06c322c8568ae2a36ab61a872e2dce5d4724
BoatThatBreached <miskam1345@gmail.com>
2022-04-17 22:48:52 +0500

    window

--- editor.py
+++ editor.py
@@ -1,1 +1,16 @@
-print('this is the editor')
+#print('this is the editor')
+
+from PyQt6.QtWidgets import QApplication, QWidget
+# Только для доступа к аргументам командной строки
+
+# Приложению нужен один (и только один) экземпляр QApplication.
+# Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
+# Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
+app = QApplication([])
+
+# Создаём виджет Qt — окно.
+window = QWidget()
+window.show()  # Важно: окно по умолчанию скрыто.
+
+# Запускаем цикл событий.
+app.exec()

251a8d97ebf0e6aabd02bd24544c327c5a620221
BoatThatBreached <miskam1345@gmail.com>
2022-04-18 13:31:55 +0500

    added graphs visualization

--- editor.py
+++ editor.py
@@ -25,7 +25,9 @@ class Example(QWidget):
         self.lines = []
         self.lastPos = self.getPoint(0, 0)
         self.lineDot = None
-
+        self.currPoint = None
+        self.currLines = []
+        self.whichPoints = []
         super().__init__()
         self.initUI()
 
@@ -41,22 +43,49 @@ class Example(QWidget):
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
-        self.drawPoints(qp)
+        #self.drawPoints(qp)
         self.drawLines(qp)
-        qp.setPen(QPen(Qt.GlobalColor.red, 8))
         qp.end()
 
     def mousePressEvent(self, e):
         self.lastPos = e.position()
         self.lastButton = e.button()
         if self.lastButton == Qt.MouseButton.RightButton:
-            if not self.lineDot:
-                self.lineDot = e.position()
-            else:
+            if self.lineDot:
                 self.lines.append(self.getLine(self.lineDot, e.position()))
-                self.lineDot = None
+            self.lineDot = e.position()
+##            if not self.lineDot:
+##                self.lineDot = e.position()
+##            else:
+##                self.lines.append(self.getLine(self.lineDot, e.position()))
+##                self.lineDot = None
+        elif self.lastButton == Qt.MouseButton.LeftButton:
+            self.lineDot = None
+            def dist(p1, p2):
+                return ((p1.x()-p2.x())**2+(p1.y()-p2.y())**2)**0.5
+            p = e.position()
+            for line in self.lines:
+                p1 = line.p1()
+                p2 = line.p2()
+                if dist(p, p1)<=16:
+                    #self.lines.remove(line)
+                    self.whichPoints.append(True)
+                    self.currLines.append(line)
+                elif dist(p, p2)<=16:
+                    #self.lines.remove(line)
+                    self.whichPoints.append(False)
+                    self.currLines.append(line)
+            for line in self.currLines:
+                self.lines.remove(line)
+                    
         self.update()
+    def mouseReleaseEvent(self, e):
+        if self.currLines:
+            self.lines.extend(self.currLines)
+        self.currLines = []
+        self.whichPoints = []
 
+        
     def wheelEvent(self, e):
         p = e.position()
         coeff = 1 + e.angleDelta().y() / 3600
@@ -72,7 +101,11 @@ class Example(QWidget):
 
     def mouseMoveEvent(self, e):
         if self.lastButton == Qt.MouseButton.LeftButton:
-            self.dots.append(e.position())
+            if self.currLines:
+                for i in range(len(self.currLines)):
+                    self.currLines[i] = self.getLine(e.position(), self.currLines[i].p2()) if self.whichPoints[i] else  self.getLine(self.currLines[i].p1(), e.position())
+
+            #self.dots.append(e.position())
         if self.lastButton == Qt.MouseButton.MiddleButton:
             offset = e.position() - self.lastPos
             self.dots = list(map(lambda t: t + offset, self.dots))
@@ -91,10 +124,20 @@ class Example(QWidget):
 
     def drawLines(self, qp):
         #TODO: optimize lines
-        qp.setPen(QPen(Qt.GlobalColor.red, 5))
+        qp.setPen(QPen(Qt.GlobalColor.red, 4))
         for d in self.lines:
             qp.drawLine(d)
-
+        qp.setPen(QPen(Qt.GlobalColor.blue, 6))
+        for d in self.lines:
+            qp.drawEllipse(d.p1().x()-3, d.p1().y()-3, 6, 6)   
+            qp.drawEllipse(d.p2().x()-3, d.p2().y()-3, 6, 6)
+        qp.setPen(QPen(Qt.GlobalColor.red, 4))
+        for d in self.currLines:
+            qp.drawLine(d)
+        qp.setPen(QPen(Qt.GlobalColor.blue, 6))
+        for d in self.currLines:
+            qp.drawEllipse(d.p1().x()-3, d.p1().y()-3, 6, 6)   
+            qp.drawEllipse(d.p2().x()-3, d.p2().y()-3, 6, 6)
 
 if __name__ == "__main__":
     # Приложению нужен один (и только один) экземпляр QApplication.

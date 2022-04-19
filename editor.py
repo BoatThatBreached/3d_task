09119aba958a481b59fe6b06372a73d692d2746a
BoatThatBreached <miskam1345@gmail.com>
2022-04-19 22:04:34 +0500

    added simple 3d

--- editor.py
+++ editor.py
@@ -1,29 +1,16 @@
-# print('this is the editor')
-import random
-
-import PyQt6.QtCore
-from PyQt6.QtCore import Qt
+import numpy as np
+from PyQt6.QtCore import Qt, QPointF, QLineF
 from PyQt6.QtGui import QPainter, QPen
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
-
-import sys  # Только для доступа к аргументам командной строки
-
+import geom as g
 
 class Example(QWidget):
-
-    @staticmethod
-    def getLine(x, y):
-        return PyQt6.QtCore.QLineF(x, y)
-
-    @staticmethod
-    def getPoint(x, y):
-        return PyQt6.QtCore.QPointF(x, y)
-
+    
     def __init__(self):
         
         self.dots = []
         self.lines = []
-        self.lastPos = self.getPoint(0, 0)
+        self.lastPos = QPointF(0, 0)
         self.lineDot = None
         self.currPoint = None
         self.currLines = []
@@ -52,27 +39,18 @@ class Example(QWidget):
         self.lastButton = e.button()
         if self.lastButton == Qt.MouseButton.RightButton:
             if self.lineDot:
-                self.lines.append(self.getLine(self.lineDot, e.position()))
+                self.lines.append(QLineF(self.lineDot, e.position()))
             self.lineDot = e.position()
-##            if not self.lineDot:
-##                self.lineDot = e.position()
-##            else:
-##                self.lines.append(self.getLine(self.lineDot, e.position()))
-##                self.lineDot = None
         elif self.lastButton == Qt.MouseButton.LeftButton:
             self.lineDot = None
-            def dist(p1, p2):
-                return ((p1.x()-p2.x())**2+(p1.y()-p2.y())**2)**0.5
             p = e.position()
             for line in self.lines:
                 p1 = line.p1()
                 p2 = line.p2()
-                if dist(p, p1)<=16:
-                    #self.lines.remove(line)
+                if g.dist(p, p1)<=16:
                     self.whichPoints.append(True)
                     self.currLines.append(line)
-                elif dist(p, p2)<=16:
-                    #self.lines.remove(line)
+                elif g.dist(p, p2)<=16:
                     self.whichPoints.append(False)
                     self.currLines.append(line)
             for line in self.currLines:
@@ -95,7 +73,7 @@ class Example(QWidget):
             return p + d
 
         self.dots = list(map(expand, self.dots))
-        self.lines = list(map(lambda line: self.getLine(
+        self.lines = list(map(lambda line: QLineF(
             expand(line.p1()), expand(line.p2())), self.lines))
         self.update()
 
@@ -103,13 +81,12 @@ class Example(QWidget):
         if self.lastButton == Qt.MouseButton.LeftButton:
             if self.currLines:
                 for i in range(len(self.currLines)):
-                    self.currLines[i] = self.getLine(e.position(), self.currLines[i].p2()) if self.whichPoints[i] else  self.getLine(self.currLines[i].p1(), e.position())
+                    self.currLines[i] = QLineF(e.position(), self.currLines[i].p2()) if self.whichPoints[i] else  QLineF(self.currLines[i].p1(), e.position())
 
-            #self.dots.append(e.position())
         if self.lastButton == Qt.MouseButton.MiddleButton:
             offset = e.position() - self.lastPos
             self.dots = list(map(lambda t: t + offset, self.dots))
-            self.lines = list(map(lambda t: self.getLine(t.p1() + offset, t.p2() + offset), self.lines))
+            self.lines = list(map(lambda t: QLineF(t.p1() + offset, t.p2() + offset), self.lines))
 
         self.lastPos = e.position()
         self.update()
@@ -118,9 +95,8 @@ class Example(QWidget):
         size = self.size()
         qp.setPen(QPen(Qt.GlobalColor.green, 5))
 
-        for d in filter(lambda point: not (point.x() < 0 or point.x() > size.width() \
-                                      or point.y() < 0 or point.y() > size.height()), self.dots):
-            qp.drawEllipse(d.x(), d.y(), 3, 3)
+        for d in filter(lambda point: g.validPoint(point, size), self.dots):
+            qp.drawEllipse(int(d.x()), int(d.y()), 3, 3)
 
     def drawLines(self, qp):
         #TODO: optimize lines
@@ -139,13 +115,105 @@ class Example(QWidget):
             qp.drawEllipse(d.p1().x()-3, d.p1().y()-3, 6, 6)   
             qp.drawEllipse(d.p2().x()-3, d.p2().y()-3, 6, 6)
 
+class Editor(QWidget):
+    def __init__(self):
+
+        self.right = np.array([1., 0., 0.])
+        self.up = np.array([0., 1., 0.])
+        self.axis = [np.array([50.,0.,0.]),np.array([0.,50.,0.]),np.array([0.,0.,50.])]
+        
+        self.initFigures()
+        self.width= 5
+        super().__init__()
+        self.initUI()
+        
+    def initFigures(self):
+        self.points = [np.array([20,20,20]),
+                       np.array([20,20,10]),
+                       np.array([10,20,20]),
+                       np.array([20,10,20]),
+                       np.array([20,10,10]),
+                       np.array([10,20,10]),
+                       np.array([10,10,20]),
+                       np.array([10,10,10])]
+        self.lines = []
+        def connect(p1, p2):
+            self.lines.append((self.points[p1], self.points[p2]))
+        for i in 2,3,7:
+            connect(6,i)
+        for i in 1,3,7:
+            connect(4,i)
+        for i in 1,2,3:
+            connect(0,i)
+        for i in 2,7,1:
+            connect(5, i)
+        
+        
+    def initUI(self):
+
+        self.setGeometry(640, 480, 640, 480)
+        self.setWindowTitle('3D Editor')
+        self.center = QPointF(320, 240)
+        self.show()
+    
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        
+        self.drawAxis(qp)
+        self.drawLines(qp)
+        self.drawPoints(qp)
+        
+        qp.end()
+    def wheelEvent(self, e):        
+        self.width += e.angleDelta().y() / 1000
+        self.update()
+
+    def mousePressEvent(self, e):
+        self.lastPos = e.position()
+        self.lastButton = e.button()                   
+        self.update()
+    def mouseMoveEvent(self, e):
+        if self.lastButton == Qt.MouseButton.LeftButton:
+            
+            delta = e.position()-self.lastPos
+            alpha = delta.x()/360
+            beta = delta.y()/360
+            
+            self.up = g.turnAroundX(beta, self)
+            self.right = g.turnAroundY(alpha, self)
+            
+        if self.lastButton==Qt.MouseButton.MiddleButton:
+            self.center+=e.position()-self.lastPos
+        self.lastPos = e.position()
+        self.update()    
+    def drawAxis(self, qp):
+        
+        cols = [Qt.GlobalColor.red,Qt.GlobalColor.yellow,Qt.GlobalColor.green]
+        
+        for a in self.axis:
+            p = g.translatePoint(a, self)
+            qp.setPen(QPen(cols[0], 2))
+            line = QLineF(self.center, self.center+p)
+            qp.drawLine(line)
+            cols.append(cols.pop(0))
+            
+    def drawPoints(self, qp):
+        for p in self.points:
+            p = g.translatePoint(p, self)
+            qp.setPen(QPen(Qt.GlobalColor.blue, 2))
+            qp.drawEllipse(self.center+p, 3,3)
+            
+    def drawLines(self, qp):
+        for t in self.lines:
+            p1 = g.translatePoint(t[0], self)
+            p2 = g.translatePoint(t[1], self)
+            qp.setPen(QPen(Qt.GlobalColor.cyan, 2))
+            qp.drawLine(QLineF(self.center+p1, self.center+p2))
+            
+        
 if __name__ == "__main__":
-    # Приложению нужен один (и только один) экземпляр QApplication.
-    # Передаём sys.argv, чтобы разрешить аргументы командной строки для приложения.
-    # Если не будете использовать аргументы командной строки, QApplication([]) тоже работает
-    app = QApplication(sys.argv)
-
-    # Создаём виджет Qt — окно.
-    ex = Example()
-
+    app = QApplication([])
+    #ex = Example()
+    ex2 = Editor()
     app.exec()
--- geom.py
+++ geom.py
@@ -1,2 +1,34 @@
-import time
-print("hello it's {0}".format(time.ctime()))
+from PyQt6.QtCore import Qt, QPointF, QLineF
+import numpy as np
+
+def dist(p1, p2):
+    return ((p1.x()-p2.x())**2+(p1.y()-p2.y())**2)**0.5
+
+def validPoint(point, size):
+    return 0<=point.x()<=size.width() and 0<=point.y()<=size.height()
+def getProjection(a,b):
+    return np.dot(a,b)/np.dot(b,b)
+def withLength(x, h):
+    return x/np.linalg.norm(x)*h
+def translatePoint(p, editor):
+    px = getProjection(p, withLength(editor.right, 1/editor.width))
+    py = getProjection(p, withLength(editor.up, 1/editor.width))
+    return QPointF(px, py)
+def turnAroundX(beta, editor):
+    length = np.linalg.norm(editor.up)
+    cosA = editor.up[1]/length
+    cosB = np.cos(beta)
+    sinA = editor.up[2]/length
+    sinB = np.sin(beta)
+    cosAB = cosA*cosB-sinA*sinB
+    sinAB = sinA*cosB+sinB*cosA
+    return np.array([0, length*cosAB, length*sinAB])
+def turnAroundY(alpha, editor):
+    length = np.linalg.norm(editor.right)
+    cosA = editor.right[0]/length
+    cosB = np.cos(alpha)
+    sinA = editor.right[2]/length
+    sinB = np.sin(alpha)
+    cosAB = cosA*cosB-sinA*sinB
+    sinAB = sinA*cosB+sinB*cosA
+    return np.array([length*cosAB, 0, length*sinAB])
